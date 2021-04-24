cc43a763744333c1ee4d9a7341e7611cb7f7ed3b
Nitipat <jackolentern01@gmail.com>
2021-04-24 20:36:57 +0700

    user_register
    กำลังเพิ่ม user_login

---
+++ backend/routes/user_login.js
@@ -0,0 +1,3 @@
+const Bcrypt = require("bcryptjs");
+const router = require('express').Router();
+let User = require('../models/user.model');
---
+++ backend/routes/user_register.js
@@ -0,0 +1,62 @@
+const Bcrypt = require("bcryptjs");
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => { // Get All User
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => { // Add User
+  const username = req.body.username;
+  const password = Bcrypt.hashSync(req.body.password);
+  const email = req.body.email;
+  const roles = Number(req.body.roles);
+  const address = "";
+  const Tel = "";
+
+  const newUser = new User({
+    username,
+    password,
+    email,
+    roles,
+    address,
+    Tel
+  });
+
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => { // get user by id
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => { // delete user by id
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User deleted!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => { // Update & edit User
+  User.findById(req.params.id)
+    .then(user => {
+      user.username = req.body.username;
+      user.password = req.body.password;
+      user.email = req.body.email;
+      user.address = req.body.address;
+      user.Tel = req.body.Tel;
+      user.save()
+        .then(() => res.json('User updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+
+module.exports = router;
