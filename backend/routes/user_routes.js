c0f8a9051c1cf8fef7be92bb5083ea9809e89323
Nitipat <62010494@kmitl.ac.th>
2021-05-05 07:33:55 +0700

    Fix!

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -32,7 +32,7 @@ router.route('/').get((req, res) => { // Get All User
 router.route('/register').post( // register User
   validSign,registerController);
 
-router.post('/activation/:token', activationController) // activate user
+router.get('/activation/:token', activationController) // activate user
 
 router.route('/login').post( // Add User
   validLogin,signinController);
